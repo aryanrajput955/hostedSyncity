0d16b07d2666e566ab2c605d5a7b7663cab92845
aryanrajput955 <aryan692000@gmail.com>
2026-03-10 17:09:51 +0530

    sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -74,12 +74,6 @@ export default function sitemap() {
       changeFrequency: "yearly",
       priority: 0.3,
     },
-    {
-      url: `${baseUrl}/terms`,
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.3,
-    },
   ];
 
   return staticPages;
