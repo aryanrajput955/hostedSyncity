b533c4cff6633608df75b291d253f796bf3ef780
aryanrajput955 <aryan692000@gmail.com>
2026-03-22 11:42:36 +0530

    canocical fixed

--- app/robots.js
+++ app/robots.js
@@ -4,7 +4,6 @@ export default function robots() {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/api/"],
       },
     ],
     sitemap: "https://syncityevents.com/sitemap.xml",
